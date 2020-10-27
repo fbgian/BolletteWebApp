b9527eabe5ea298ad3ef4c478ab7a6f27e1c86e4
antlanc7 <antonio.lanciotti@live.it>
2020-10-28 00:34:42 +0100

    Update Debtors.php

--- app/Models/Debtors.php
+++ app/Models/Debtors.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Debtors extends Model
 {
-    //
+    protected $table = 'debtors';
+    protected $primaryKey = 'id';
+    protected $guarded = ['id'];
+    public $timestamps = false;
 
 
     // Relazione con i debitori
