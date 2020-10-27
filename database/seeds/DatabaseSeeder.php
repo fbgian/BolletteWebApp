d4d53d7dff16be37754f78c3d2bb9468cb41ba77
fbgian <fgchaka@gmail.com>
2020-10-28 00:41:15 +0100

    Aggiornato dbseeder con debtors

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        DB::table('Debtors')->insert([
+            ['id' => 1, 'name' => 'Fabio', ],
+            ['id' => 2, 'name' => 'Antonio'],
+            ['id' => 3, 'name' => 'Vito'],
+            ['id' => 4, 'name' => 'Massimo'],
+            ['id' => 5, 'name' => 'Karim'],
+        ]);
     }
 }
