1450ea35fdc3bdbd992c821decc03e6307794b0b
fbgian <fgchaka@gmail.com>
2020-10-28 00:19:52 +0100

    aggiunte migrations

--- database/migrations/2014_10_12_000000_create_bills_table.php
+++ database/migrations/2014_10_12_000000_create_bills_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateBillsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('username');
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('bills', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned()->index();
+            $table->string('name');
+            $table->double('price');
+            $table->string('period');
+            $table->string('notes');
         });
     }
 
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bills');
     }
 }
